acef41cb029d5e6d8b1ee74b14ddafe4bfb73fac
Juan David Castro <me@juandc.co>
2022-05-23 13:53:59 -0500

    escribir html desde js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('#pid');
+const parrafito = document.getElementsByClassName('parrafito');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log(input.value);
@@ -17,3 +17,23 @@ console.log({
   pid,
   input,
 });
+
+h1.innerHTML = 'Patito <br> Feo';
+h1.innerText = 'Patito <br> Feo';
+// console.log(h1.getAttribute('class'));
+// h1.setAttribute('class', 'rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+input.value = "456"
+
+const img = document.createElement('img');
+img.setAttribute('src', 'https://static.platzi.com/static/images/conf/logo_black@2x.png');
+console.log(img);
+
+pid.innerHTML = "";
+pid.appendChild(img);
+
