6759d6dcc7fc86e243a1217010b18a522dec088f
Juan David Castro <me@juandc.co>
2022-07-18 08:36:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form'); 
 const input1 = document.querySelector('#calculo1');
 const input1 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-function btnOnClick() {
+// form.addEventListener('submit', sumarInputValues)
+
+// function sumarInputValues(event) {
+//   // console.log({event});
+//   event.preventDefault();
+//   const sumaInputs = input1.value + input2.value;
+//   pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+btn.addEventListener('click', sumarInputValues)
+
+function sumarInputValues(event) {
+  // console.log({event});
+  // event.preventDefault();
   const sumaInputs = input1.value + input2.value;
   pResult.innerText = "Resultado: " + sumaInputs;
 }
