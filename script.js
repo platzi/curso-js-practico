ca31208d312428412eca89c7fab43ebd2968596c
Juan David Castro <me@juandc.co>
2022-07-18 08:31:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
-// h1 { color: red }
-// .parrafito { ... }
-// #pid { ... }
-
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input1 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
-console.log(input.value);
-
-console.log({
-  h1,
-  p,
-  parrafito,
-  pid,
-  input,
-});
-
-h1.innerHTML = 'Patito <br> Feo';
-h1.innerText = 'Patito <br> Feo';
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo');
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-// h1.classList.toggle('verde');
-// h1.classList.contains('verde');
-
-input.value = "456"
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://static.platzi.com/static/images/conf/logo_black@2x.png');
-console.log(img);
-
-pid.innerHTML = "";
-pid.appendChild(img);
-
+function btnOnClick() {
+  const sumaInputs = input1.value + input2.value;
+  pResult.innerText = "Resultado: " + sumaInputs;
+}
