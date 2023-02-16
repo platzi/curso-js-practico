02d9db5209da0f9b6f8a5cc8a2c36b8ffc0865e8
JeisonPC <jeida16@yahoo.es>
2023-02-15 19:14:59 -0500

    Agregando archivos de clase 10

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello, world parce');
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.querySelector('.parrafito');
+const pid = document.querySelector('#pid');
+const input = document.querySelector('input');
+console.log(input.value);
+
+
+console.log({
+    h1,
+    p,
+    parrafito,
+    pid,
+    input
+}
+);
