6fa39a8a6c14ac362d864a3c4b3a9233c632f8d1
JeisonPC <jeida16@yahoo.es>
2023-02-16 19:21:35 -0500

    Agregando cambios de la clase 12

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Planes Raros
+---
+## Primeras Opciones
+
+1. Diseñar CSS para calculadora
+2. trabajar en el funcionamiento de la misma
+3. Presentar las ideas lokas
+
+1.1 
+- El css sera suave con curvas y adaptable.}
+- Colores claros 
+- Letra fuerte
--- script.js
+++ script.js
@@ -1,40 +1,17 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.querySelector('.parrafito');
-const pid = document.querySelector('#pid');
-const input = document.querySelector('input');
-console.log(input.value);
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const bnt = document.querySelector('#btnCalcular');
+const parrafo = document.getElementById('result');
 
-
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
+function btnCalcular() {
+    
+    const resultado = input1.value + input2.value;
+    
+    parrafo.innerText = "Resultado: " + resultado;
+    
 }
-);
 
-h1.innerText = 'Quiubo pues ome!!<br> Como fue con innerText'
-h1.innerHTML = 'Quiubo pues ome!!<br> Como fue con innerHTML'
 
-// p.getAttribute("pantalla")
-// console.log(p.getAttribute('pantalla'));
-// console.log(p.getAttribute('class'));
-// p.setAttribute('class', 'wisp')
 
-p.classList.add('rojo');
-p.classList.remove('redDragon');
-// p.classList.toggle('redDragon');
-// p.classList.contains('redDragon');
 
-input.value = "Jeison";
-
-// document.createElement('img')
-// console.log(document.createElement('img'));
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://i1.wp.com/legundo.com/wp-content/uploads/2018/07/download.jpg?zoom=1.25&fit=1920%2C1080&ssl=1');
-img.setAttribute('width', '200px');
-img.setAttribute('height', '100px');
-pid.append(img);
