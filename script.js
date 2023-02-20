2b7f792afdbd328cc2dc2b0df2b1405015910bd0
JeisonPC <jeida16@yahoo.es>
2023-02-20 17:43:50 -0500

    Agregando archivos de la clase 13

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const h1 = document.querySelector('h1');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
-const bnt = document.querySelector('#btnCalcular');
+const btn = document.querySelector('#btnCalcular');
 const parrafo = document.getElementById('result');
+const form = document.querySelector('#form');
 
-function btnCalcular() {
-    
+form.addEventListener('submit', btnCalcular)
+
+function btnCalcular(event) {
     const resultado = input1.value + input2.value;
-    
+    event.preventDefault()
     parrafo.innerText = "Resultado: " + resultado;
     
 }
+
 
 
 
